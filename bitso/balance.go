26691b5e1268403b38bb50b763dfb6f949ddf93f
José Nieto <jose@xiam.io>
2023-12-23 06:53:43 -0600

    add pending_deposit and pending_withdrawal

--- bitso/balance.go
+++ bitso/balance.go
@@ -2,8 +2,10 @@ package bitso
 
 // Balance represents the balance of a given currency.
 type Balance struct {
-	Currency  Currency `json:"currency"`
-	Total     Monetary `json:"total"`
-	Locked    Monetary `json:"locked"`
-	Available Monetary `json:"available"`
+	Currency          Currency `json:"currency"`
+	Total             Monetary `json:"total"`
+	Locked            Monetary `json:"locked"`
+	Available         Monetary `json:"available"`
+	PendingDeposit    Monetary `json:"pending_deposit"`
+	PendingWithdrawal Monetary `json:"pending_withdrawal"`
 }
