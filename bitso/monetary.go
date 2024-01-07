9e49f54d60230bbe38c6b0d1c8a3227bc9bf9dcc
José Nieto <jose@xiam.io>
2024-01-06 19:50:32 -0600

    add missing currencies

--- bitso/monetary.go
+++ bitso/monetary.go
@@ -11,13 +11,13 @@ import (
 type Monetary string
 
 // Float64 returns the monetary value as a float64
-func (m *Monetary) Float64() float64 {
-	v, _ := strconv.ParseFloat(string(*m), 64)
+func (m Monetary) Float64() float64 {
+	v, _ := strconv.ParseFloat(string(m), 64)
 	return v
 }
 
-func (m *Monetary) Decimal() (decimal.Decimal, error) {
-	return decimal.NewFromString(string(*m))
+func (m Monetary) Decimal() (decimal.Decimal, error) {
+	return decimal.NewFromString(string(m))
 }
 
 // ToMonetary converts a float64 value into Monetary
