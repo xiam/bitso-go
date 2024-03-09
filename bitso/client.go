6a1e351fa5300fe4967a908d07ddcdc2cb968660
José Nieto <jose@xiam.io>
2024-03-09 13:45:11 -0600

    fix leaking ticker

--- bitso/client.go
+++ bitso/client.go
@@ -14,6 +14,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -55,6 +56,8 @@ type Client struct {
 	tickets chan struct{}
 
 	burstRate time.Duration
+
+	mu sync.RWMutex
 }
 
 // NewClient creates and returns a new Bitso API client.
@@ -82,6 +85,9 @@ func (c *Client) endpointURL(endpoint string) (*url.URL, error) {
 
 // SetLogLevel sets the log level for the client.
 func (c *Client) SetLogLevel(level zerolog.Level) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.logger = c.logger.Level(level)
 }
 
@@ -93,154 +99,18 @@ func (c *Client) SetAuth(key, secret string) {
 
 // SetAPIBaseURL sets the API prefix
 func (c *Client) SetAPIBaseURL(prefix string) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.baseURL = strings.TrimRight(prefix, "/") + "/"
 }
 
 // APIBaseURL returns the API prefix
 func (c *Client) APIBaseURL() string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	return c.baseURL
-}
-
-func (c *Client) newRequest(logger *zerolog.Logger, method string, uri string, body io.Reader) (*http.Request, error) {
-	var buf []byte
-
-	if body != nil {
-		var err error
-		buf, err = ioutil.ReadAll(body)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	req, err := http.NewRequest(method, uri, bytes.NewBuffer(buf))
-	if err != nil {
-		return nil, err
-	}
-
-	if method == "POST" {
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Set("Accept", "application/json")
-	}
-
-	if c.key == "" && c.secret == "" {
-		// Return unsigned request
-		return req, nil
-	}
-
-	u, err := url.Parse(uri)
-	if err != nil {
-		return nil, err
-	}
-
-	nonce := time.Now().UnixNano()
-	message := strconv.FormatInt(nonce, 10) + method + u.RequestURI() + string(buf)
-
-	mac := hmac.New(sha256.New, []byte(c.secret))
-	mac.Write([]byte(message))
-
-	signature := fmt.Sprintf("%x", mac.Sum(nil))
-
-	authHeader := fmt.Sprintf("Bitso %s:%d:%s", c.key, nonce, signature)
-	req.Header.Set("Authorization", authHeader)
-
-	if logger.GetLevel() <= zerolog.TraceLevel {
-		*logger = logger.With().
-			Str("auth", authHeader).
-			Str("message", message).
-			Str("signature", signature).
-			Logger()
-	}
-
-	return req, nil
-}
-
-func (c *Client) doRequest(method string, endpoint string, params url.Values, body io.Reader, dest interface{}) error {
-	logger := c.logger.With().
-		Str("method", method).
-		Str("endpoint", endpoint).
-		Logger()
-
-	u, err := c.endpointURL(endpoint)
-	if err != nil {
-		return err
-	}
-	u.RawQuery = params.Encode()
-
-	req, err := c.newRequest(&logger, method, u.String(), body)
-	if err != nil {
-		return err
-	}
-
-	// Apply burst-rate protection.
-	if br := c.burstRate; br > 0 {
-		<-c.tickets
-		ticker := time.NewTicker(br)
-		defer ticker.Stop()
-		go func() {
-			<-ticker.C
-			c.tickets <- struct{}{}
-		}()
-	}
-
-	res, err := c.client.Do(req)
-	if err != nil {
-		logger.Error().Err(err).Msg("request failed")
-		return err
-	}
-	defer res.Body.Close()
-
-	buf, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		logger.Error().Err(err).Msg("can not read response body")
-		return err
-	}
-
-	logger = logger.With().
-		Int("status", res.StatusCode).
-		Logger()
-
-	if logger.GetLevel() <= zerolog.DebugLevel {
-		logger = logger.With().
-			Str("body", string(buf)).
-			Logger()
-	}
-
-	var env Envelope
-	if err := json.Unmarshal(buf, &env); err != nil {
-		logger.Error().Msg("can not unmarshal envelope")
-		return err
-	}
-
-	if !env.Success {
-		code, _ := strconv.Atoi(fmt.Sprintf("%v", env.Error.Code))
-		logger.Error().
-			Int("error.code", code).
-			Msgf("api error: %s", env.Error.Message)
-		return apiError(code, env.Error.Message)
-	}
-
-	if err := json.Unmarshal(buf, dest); err != nil {
-		logger.Error().Msg("can not unmarshal payload")
-		return err
-	}
-
-	return nil
-}
-
-func (c *Client) deleteResponse(endpoint string, params url.Values, dest interface{}) error {
-	return c.doRequest("DELETE", endpoint, params, nil, dest)
-}
-
-func (c *Client) getResponse(endpoint string, params url.Values, dest interface{}) error {
-	return c.doRequest("GET", endpoint, params, nil, dest)
-}
-
-func (c *Client) postResponse(endpoint string, body interface{}, dest interface{}) error {
-	buf, err := json.Marshal(body)
-	if err != nil {
-		return err
-	}
-	return c.doRequest("POST", endpoint, nil, bytes.NewBuffer(buf), dest)
 }
 
 // AvailableBooks returns a list of existing exchange order books and their
@@ -428,7 +298,7 @@ func (c *Client) LookupOrders(oids []string) ([]UserOrder, error) {
 	res := struct {
 		Payload []UserOrder `json:"payload"`
 	}{}
-	if err := c.getResponse("/orders/"+strings.Join(oids, "-"), nil, &res); err != nil {
+	if err := c.getResponse("/orders/"+strings.Join(oids, ","), nil, &res); err != nil {
 		return nil, err
 	}
 	return res.Payload, nil
@@ -439,7 +309,7 @@ func (c *Client) CancelOrders(oids []string) ([]string, error) {
 	var res struct {
 		Payload []string `json:"payload"`
 	}
-	if err := c.deleteResponse("/orders/"+strings.Join(oids, "-"), nil, &res); err != nil {
+	if err := c.deleteResponse("/orders/"+strings.Join(oids, ","), nil, &res); err != nil {
 		return nil, err
 	}
 	return res.Payload, nil
@@ -466,11 +336,161 @@ func (c *Client) PlaceOrder(order *OrderPlacement) (string, error) {
 
 // BurstRate returns the current burst-rate limit.
 func (c *Client) BurstRate() time.Duration {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+
 	return c.burstRate
 }
 
 // SetBurstRate sets the amount of time the client should wait in between
 // requests.
 func (c *Client) SetBurstRate(burstRate time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.burstRate = burstRate
+}
+
+func (c *Client) newRequest(logger *zerolog.Logger, method string, uri string, body io.Reader) (*http.Request, error) {
+	var buf []byte
+
+	if body != nil {
+		var err error
+		buf, err = ioutil.ReadAll(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, uri, bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, err
+	}
+
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Accept", "application/json")
+	}
+
+	if c.key == "" && c.secret == "" {
+		// Return unsigned request
+		return req, nil
+	}
+
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := time.Now().UnixNano()
+	message := strconv.FormatInt(nonce, 10) + method + u.RequestURI() + string(buf)
+
+	mac := hmac.New(sha256.New, []byte(c.secret))
+	mac.Write([]byte(message))
+
+	signature := fmt.Sprintf("%x", mac.Sum(nil))
+
+	authHeader := fmt.Sprintf("Bitso %s:%d:%s", c.key, nonce, signature)
+	req.Header.Set("Authorization", authHeader)
+
+	if logger.GetLevel() <= zerolog.TraceLevel {
+		*logger = logger.With().
+			Str("auth", authHeader).
+			Str("message", message).
+			Str("signature", signature).
+			Logger()
+	}
+
+	return req, nil
+}
+
+func (c *Client) doRequest(method string, endpoint string, params url.Values, body io.Reader, dest interface{}) error {
+	logger := c.logger.With().
+		Str("method", method).
+		Str("endpoint", endpoint).
+		Logger()
+
+	u, err := c.endpointURL(endpoint)
+	if err != nil {
+		return err
+	}
+	u.RawQuery = params.Encode()
+
+	req, err := c.newRequest(&logger, method, u.String(), body)
+	if err != nil {
+		return err
+	}
+
+	// Apply burst-rate protection.
+	if burstRate := c.BurstRate(); burstRate > 0 {
+		<-c.tickets
+		ticker := time.NewTicker(burstRate)
+
+		go func() {
+			<-ticker.C
+			ticker.Stop()
+
+			c.tickets <- struct{}{}
+		}()
+	}
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		logger.Error().Err(err).Msg("request failed")
+		return err
+	}
+	defer res.Body.Close()
+
+	buf, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		logger.Error().Err(err).Msg("can not read response body")
+		return err
+	}
+
+	logger = logger.With().
+		Int("status", res.StatusCode).
+		Logger()
+
+	if logger.GetLevel() <= zerolog.DebugLevel {
+		logger = logger.With().
+			Str("body", string(buf)).
+			Logger()
+	}
+
+	var env Envelope
+	if err := json.Unmarshal(buf, &env); err != nil {
+		logger.Error().Msg("can not unmarshal envelope")
+		return err
+	}
+
+	if !env.Success {
+		code, _ := strconv.Atoi(fmt.Sprintf("%v", env.Error.Code))
+		logger.Error().
+			Int("error.code", code).
+			Msgf("api error: %s", env.Error.Message)
+		return apiError(code, env.Error.Message)
+	}
+
+	if err := json.Unmarshal(buf, dest); err != nil {
+		logger.Error().Msg("can not unmarshal payload")
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) deleteResponse(endpoint string, params url.Values, dest interface{}) error {
+	return c.doRequest("DELETE", endpoint, params, nil, dest)
+}
+
+func (c *Client) getResponse(endpoint string, params url.Values, dest interface{}) error {
+	return c.doRequest("GET", endpoint, params, nil, dest)
+}
+
+func (c *Client) postResponse(endpoint string, body interface{}, dest interface{}) error {
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+	return c.doRequest("POST", endpoint, nil, bytes.NewBuffer(buf), dest)
 }
